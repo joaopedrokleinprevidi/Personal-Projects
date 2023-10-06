d3aa70445db50a86627d0449e8e5ff17a4adb01b
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-10-06 20:04:35 -0300

    Finish project jokempo

--- Jokempô Project/script.js
+++ Jokempô Project/script.js
@@ -4,18 +4,6 @@ function playGame() {
     window.location = "http://127.0.0.1:5500/Jokemp%C3%B4%20Project/game%20archives/game.html";
 }
 
-var loading_all = document.querySelector(".loading_all");
-function loading_false(){
-    loading_all.classList.remove('loadingPedraTrue');
-    loading_all.classList.remove('loadingPapelTrue');
-    loading_all.classList.remove('loadingTesouraTrue');
-    loading_all.classList.add('loadingPedraFalse')
-    loading_all.classList.add('loadingPapelFalse')
-    loading_all.classList.add('loadingTesouraFalse')
-    }
-    loading_false();
-    console.log("É pra estar tudo false: " + loading_all.classList)
-
 var selectPedra = document.querySelector("#selectPedra");
 var selectPapel = document.querySelector("#selectPapel");
 var selectTesoura = document.querySelector("#selectTesoura");
@@ -33,11 +21,10 @@ var defeatHTML = document.querySelector('#defeat');
 var empateHTML = document.querySelector('#empate');
 var scoreHTML = document.querySelector('#score');
 
-function MaquinaG(){
-var aleatorio = parseInt(Math.random() * 3);
-return aleatorio;
-}
-MaquinaG();
+function maquinaJogou(){
+var maquinaAleatorio = parseInt(Math.random() * 3);
+return maquinaAleatorio;
+};
 
 function resetInterface(){
     win = 0;
@@ -49,12 +36,9 @@ function resetInterface(){
     defeatHTML.textContent = "Derrota: " + defeat;
     winHTML.textContent = "Vitória: " + win;
     scoreHTML.textContent = "Score: " + score;
-// tem q ver isso aqui, pq tá bugando
-// tu da reset e ok, ele reseta, mas se tu continuar jogando ele volta como tava ante
 }
 console.log("Vitória: " + win)
 console.log("Derrota: " + defeat)
-
 
 function displayMaquina(){
     sectionMaquina.classList.remove('maquina') //tira o display none
@@ -80,131 +64,86 @@ function maquinaTesoura(){
 
 
 function usuarioPedra() {
-    if (MaquinaG() == 0){
+    var escolhaMaquina = maquinaJogou();
+    if (escolhaMaquina == 0){
         maquinaPedra();
         empate = empate + 1;
         empateHTML.textContent = "Empate: " + empate;
+        score = 0;
+        scoreHTML.textContent = "Score: " + score;
     }
-    else if (MaquinaG() == 1){
+    else if (escolhaMaquina == 1){
         maquinaPapel();
         defeat = defeat + 1;
         defeatHTML.textContent = "Derrota: " + defeat;
+        score = 0;
+        scoreHTML.textContent = "Score: " + score;
     }
-    else if (MaquinaG() == 2){
+    else if (escolhaMaquina == 2){
         maquinaTesoura();
         win = win + 1;
         winHTML.textContent = "Vitória: " + win;
         score = score + 1;
         scoreHTML.textContent = "Score: " + score;
     }
-    return true;
 }
 
 function usuarioPapel(){
-    if(MaquinaG() == 0){
+    var escolhaMaquina = maquinaJogou();
+    if(escolhaMaquina == 0){
             maquinaPedra();
             win = win + 1;
             winHTML.textContent = "Vitória: " + win;
             score = score + 1;
             scoreHTML.textContent = "Score: " + score;
     }
-    else if (MaquinaG() == 1){
+    else if (escolhaMaquina == 1){
             maquinaPapel();
             empate = empate + 1;
             empateHTML.textContent = "Empate: " + empate;
+            score = 0;
+            scoreHTML.textContent = "Score: " + score;
     }
-    else if(MaquinaG() == 2){
+    else if(escolhaMaquina == 2){
             maquinaTesoura();
             defeat = defeat + 1;
             defeatHTML.textContent = "Derrota: " + defeat;
+            score = 0;
+            scoreHTML.textContent = "Score: " + score;
     }
-    return true;
 }
 
 function usuarioTesoura(){
-    if(MaquinaG() == 0){
+    var escolhaMaquina = maquinaJogou();
+    if(escolhaMaquina == 0){
         maquinaPedra();
         defeat = defeat + 1;
         defeatHTML.textContent = "Derrota: " + defeat;
+        score = 0;
+        scoreHTML.textContent = "Score: " + score;
      }
-     else if (MaquinaG() == 1){     
+     else if (escolhaMaquina == 1){     
         maquinaPapel();
         win = win + 1;
         winHTML.textContent = "Vitória: " + win;
         score = score + 1;
         scoreHTML.textContent = "Score: " + score;
      }
-     else if(MaquinaG() == 2){
+     else if(escolhaMaquina == 2){
         maquinaTesoura();
         empate = empate + 1;
         empateHTML.textContent = "Empate: " + empate;
+        score = 0;
+        scoreHTML.textContent = "Score: " + score;
      }
-     return true;
-}
-
-function clickUsuario_loading(){
-    if (usuarioPedra() == true){
-        loading_all.classList.remove("loadingPapelFalse");
-        loading_all.classList.add("loadingPapelTrue");
-
-        loading_all.classList.remove("loadingTesouraFalse");
-        loading_all.classList.add("loadingTesouraTrue");
-        console.log("usuarioPedra(PapelTrue, TesouraTrue):" + loading_all.className)
     }
-    else if (usuarioPapel() == true){
-        loading_all.classList.remove("loadingPedraFalse");
-        loading_all.classList.add("loadingPedraTrue");
-
-        loading_all.classList.remove("loadingTesouraFalse");
-        loading_all.classList.add("loadingTesouraTrue");
-        console.log("usuarioPapel(PedraTrue, TesouraTrue):" + loading_all.className)
-    }
-    else if (usuarioTesoura() == true){
-        loading_all.classList.remove("loadingPedraFalse");
-        loading_all.classList.add("loadingPedraTrue");
-
-        loading_all.classList.remove("loadingPapelFalse");
-        loading_all.classList.add("loadingPapelTrue");
-        console.log("usuarioTesoura(PedraTrue, PapelTrue):" + loading_all.className)
-    }
-    else {
-        console.log("Deu bug aqui na função " + loading_all.classList); 
-    }
-}
-
-/*
-
-1- Deixa invisível tudo - OK
-2- IF:
-Se usuário clicar in (usuarioPedra()){
-    mostra loadingPapel && loadingTesoura
-}
-Se usuário clicar in (usuarioPapel()){
-    mostra loadingPedra && loadingTesoura
-}
-Se usuário clicar in (usuárioTesoura()){
-    mostra loadingPedra && loadingPapel
-}
-
-TEM q colocar essa função em cada jogada
-
-3- após tudo acima estiver ok. Criar animação loading
-4- Vou ter que fazer uma verificação, de caso a animação esteja OK, sumir todos os loading
-5- Ao usuário clicar novamente em uma opção, isso tem que estar pegando corretamente
-*/
-
-clickUsuario_loading();
 
 
-
-
-
-  console.log("Maquina: " + MaquinaG());
+  console.log("Maquina: " + maquinaJogou());
   console.log("Empate: "+ empate);
   console.log("Derrota: " + defeat);
   console.log("Vitória: " + win);
   console.log("Score: " + score);
-  console.log(resetInterface());
 
 /*
     RESULTADOS MÁQUINA: 
