85d59fabc081136ba132ba92cb64935688c2a885
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-10-12 19:34:46 -0300

    att project to-do-list

--- To-do-list Project/JavaScript/index.js
+++ To-do-list Project/JavaScript/index.js
@@ -51,7 +51,7 @@ button_remover.addEventListener('click', ()=> {
 })
 
 
-//index === definindo local para cada click
+//index === definindo local para cada
 list1.addEventListener('click', ()=> {
     window.location = "http://127.0.0.1:5500/To-do-list%20Project/HTML/lista1.html";
 })
