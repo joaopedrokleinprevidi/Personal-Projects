92feba3b8956acdc340c7c6f93c40d6442080d3c
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-10-18 20:37:46 -0300

    att todo project

--- To Do Project/script.js
+++ To Do Project/script.js
@@ -1,5 +1,6 @@
-var selectButtonAdicionar = document.querySelector('#adicionarButton');
-var selectForm = document.querySelector(".displayNoneFormListHeader");
+var selectButtonSubmit = document.querySelector('.submitButton');
+var selectAdicionarButton = document.querySelector('.adicionarButton');
+var selectForm = document.querySelector(".selectForm");
 var selectInputName = document.querySelector("#nameInput");
 var selectInputColor = document.querySelector("#colorInput");
 var selectInputValue = document.querySelector("#valueInput");
@@ -10,47 +11,60 @@ var inputSelectNo = document.querySelector("#inputSelectNo");
 var selectInputDate = document.querySelector("#dateInput");
 var selectDivListContainer = document.querySelector("#ListItensContainer");
 
+function criarItem(){
+    //criando item da lista e atribuindo valor nome + css pra td
+    
+    var DivItemList = document.createElement("div");
+    DivItemList.classList.add("divItemList", "itemListConcluido");
+    selectDivListContainer.appendChild(DivItemList);
+
+    var nomeArmazenado = selectInputName.value;
+
+    var p_ValoresItem = document.createElement("p");
+    p_ValoresItem.innerHTML = nomeArmazenado;
+    DivItemList.appendChild(p_ValoresItem);
+
+    //criando botoes para adicionar/editar/remover
+    var feitoButton = document.createElement("button");
+    feitoButton.classList.add('buttonsItemList', 'feitoButton');
+    DivItemList.appendChild(feitoButton);
+
+    var editarButton = document.createElement("button");
+    editarButton.classList.add('buttonsItemList', 'editarButton');
+    DivItemList.appendChild(editarButton);
+
+    var removerButton = document.createElement("button");
+    removerButton.classList.add('buttonsItemList', 'removerButton');
+    DivItemList.appendChild(removerButton);
+}
+
+
+
+selectAdicionarButton.classList.remove('esconderButton')
 selectForm.classList.remove('formListHeader');
+selectForm.classList.add('displayNoneFormListHeader');
 
 
 
-selectButtonAdicionar.addEventListener('click', ()=> {
-    //abre configurações para item da lista
-    selectForm.classList.add('formListHeader');
+selectAdicionarButton.addEventListener('click', ()=>{
 
-    selectButtonAdicionar.innerHTML = "Concluído";
-    //ao clicar em "Concluído" === cria item para lista e fornece valores recebidos
-    selectButtonAdicionar.addEventListener('click', (e)=> {
+//remove botao de adicionar, aparece painel e botao de submit
+selectAdicionarButton.classList.add('esconderButton');
+selectForm.classList.add('formListHeader');
+selectForm.classList.remove('displayNoneFormListHeader');
+selectButtonSubmit.classList.remove('esconderButton');
+
+//se o botao de submit for clicado, cria item da lista
+selectForm.addEventListener('submit', (e)=> {
         e.preventDefault();
-        selectForm.classList.remove('formListHeader');
-        selectButtonAdicionar.innerHTML = "Adicionar";
-
-//preciso pegar o valor dos inputs e colocar no "p_ValoresItem"
-
-
-        //criando item da lista
-        var DivItemList = document.createElement("div");
-        DivItemList.classList.add("divItemList");
-        selectDivListContainer.appendChild(DivItemList);
-
-        //criando paragrafo da lista, que contem as config de cada item
-        var p_ValoresItem = document.createElement("p");
-        p_ValoresItem.innerHTML = selectInputName.value;
-        DivItemList.appendChild(p_ValoresItem);
         
-        //criando botoes para adicionar/editar/remover
-        var feitoButton = document.createElement("button");
-        feitoButton.classList.add('buttonsItemList', 'feitoButton');
-        DivItemList.appendChild(feitoButton);
+        criarItem(); 
 
-        var editarButton = document.createElement("button");
-        editarButton.classList.add('buttonsItemList', 'editarButton');
-        DivItemList.appendChild(editarButton);
-
-        var removerButton = document.createElement("button");
-        removerButton.classList.add('buttonsItemList', 'removerButton');
-        DivItemList.appendChild(removerButton);
-
+        selectForm.classList.remove('formListHeader');
+        selectForm.classList.add('displayNoneFormListHeader');
+        selectAdicionarButton.classList.remove('esconderButton');
+        selectForm.reset();
     })
-
+console.log(selectInputName.value);
 })
+
