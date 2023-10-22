c1951c7578f2539013de3920887522365d7c35fd
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-10-22 09:26:02 -0300

    update app dalas delivery

--- Delivery Dalas Project/app.js
+++ Delivery Dalas Project/app.js
@@ -1,22 +1,2 @@
 import manipulador from './manipulation.js';
 import ingredientes from './ingredientes.js';
-
-//console.log(ingredientes);
-//console.log(typeof ingredientes);
-//console.log(Array.isArray(ingredientes));
-
-//console.log(ingredientes[0])
-var ingredientes = ['água', 'mel', 'sal', 'mostarda'];
-manipulador.capitalizarObjetos(ingredientes, 'nome');
-manipulador.capitalizarObjetos(ingredientes)
-
-
-
-/*
-var resultadoCapitalizado = manipulador.capitalizar(ingredientes);
-var resultadoOrdenacao = manipulador.ordenar(resultadoCapitalizado);
-var resultadoCaixaAlta = manipulador.caixaAlta(ingredientes);
-
-console.log(resultadoCapitalizado);
-console.log(resultadoOrdenacao);
-console.log(resultadoCaixaAlta); */
--- Delivery Dalas Project/manipulation.js
+++ Delivery Dalas Project/manipulation.js
@@ -1,56 +1,43 @@
-function capitalizarObjetos( colecao, atributo ) {
-    console.log(typeof colecao[0]);
-    if ( typeof colecao[0] == 'object' ) {
-        var resultado = colecao.map( function( objeto ){ 
-            var letraInicial = objeto[atributo].charAt(0).toUpperCase() + objeto[atributo].slice(1);
-            objeto[atributo] = letraInicial;
-            
-            return objeto;
-        })
-
-        console.log(resultado);
-    }
-    else {
-       var resultado = colecao.map ( function( string ) {
-            var letraInicial =  string[atributo].chartAt(0).toUpperCase + string[atributo].slice(1);
-            string[atributo] = letraInicial;
-
-            return string;
-       })
-
-       console.log(resultado);
+function fnCapitalizar(colecao, attr){
+    if (colecao.length > 0){
+        if (typeof colecao[0] == 'object' && attr){
+            colecao.map(item => item[attr] = item[attr].charAt(0).toUpperCase() + item[attr].slice(1));
+        }else{
+            colecao.map(item => item = item.charAt(0).toUpperCase() + item.slice(1));
+        }
+        
+        return colecao;
     }
 }
 
-function capitalizar(vetor) {
-    var ingredientesModificado = []
-
-    for (var i = 0; i < vetor.length; i++) {
-        var letraInicial = vetor[i].charAt(0).toUpperCase() + vetor[i].slice(1);
-        ingredientesModificado[i] = letraInicial;
+function fnCaixaAlta(colecao, attr){
+    if (colecao.length > 0){
+        if (typeof colecao[0] == 'object' && attr){
+            colecao.map(item => item[attr] = item[attr].toUpperCase());
+        }else{
+            colecao.map(item => item = item.toUpperCase());
+        }
+        
+        return colecao;
     }
-    return ingredientesModificado;
 }
 
-function ordenar(vetor) {
-    return vetor.sort(function(a,b) {
-        return a.localeCompare(b)
-    })
-    
-}
-
-function caixaAlta ( vetor ) {
-    var modificado = [];
-    for(var i = 0; i < vetor.lenght; i++){
-        modificado[i] = vetor[i].toUpperCase();
-    }
-    
-    return modificado;
+function fnOrdenar(colecao, attr){    
+    return attr ?
+        colecao.sort(function(a,b){
+            return typeof a[attr] == 'number' ?
+                a[attr] - b[attr] :
+                a[attr].localeCompare(b[attr])
+        }):
+        colecao.sort(function(a,b){
+            return typeof a == 'number' ?
+                a - b :
+                a.localeCompare(b)
+        });
 }
 
 export default {
-    capitalizar: capitalizar,
-    ordenar: ordenar,
-    caixaAlta: caixaAlta,
-    capitalizarObjetos: capitalizarObjetos
+    capitalizar: fnCapitalizar,
+    ordenar: fnOrdenar,
+    caixaAlta: fnCaixaAlta
 };
