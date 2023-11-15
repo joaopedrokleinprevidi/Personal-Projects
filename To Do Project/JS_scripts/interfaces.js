e3be45c6504e74fb795509bf50e958b325f73614
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-11-15 14:08:22 -0300

    att

--- To Do Project/JS_scripts/interfaces.js
+++ To Do Project/JS_scripts/interfaces.js
@@ -6,22 +6,19 @@ let painelEscolherListas = document.querySelector('#painelDeEscolherListas')
 let painelEscolherListas_buttonDeOcultar = document.querySelector('#painelSelectListas_ocultarButton');
 let painelEscolherListas_buttonDeInfoApresentaçao = document.querySelector('#painelSelectListas_infoButton')
 let painelLista = document.querySelector('#lists_section')
+let body = document.querySelector('.body_with_lists')
 
 function toggleOcultarOuMostrar_PainelEscolherListas() {
     painelEscolherListas.classList.toggle('painel_listasAnimationOcultar');
     painelEscolherListas_buttonDeOcultar.classList.toggle('Animation_buttonListaSelector');
-    painelEscolherListas_buttonDeInfoApresentaçao.classList.toggle('Animation_buttonListaSelector');
-    painelLista.classList.toggle('section_lists_animation');
-    painelLista.classList.toggle('section_lists');
 }
 
-
 buttonOcultarPainelEscolherListas.addEventListener('click', toggleOcultarOuMostrar_PainelEscolherListas)
-buttonInfoApresentaçao.addEventListener('click', ()=> {
-    sectionInfoApresentaçao.classList.toggle('section_apresentaçaoAnimation')
-})
+
 sectionApresentaçao_iniciarApp_button.addEventListener('click', ()=> {
     painelLista.classList.add('section_lists')
-    sectionInfoApresentaçao.classList.remove('section_apresentaçao')
+    painelEscolherListas.classList.toggle('painel-lista--visivel')
+    painelEscolherListas.classList.toggle('painel-lista')
+
 })
 
