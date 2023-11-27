d5bf653f1ec069a56fd55c79084c9ec7b99795b2
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-11-27 15:54:20 -0300

    att

---
+++ Calculadora Project/script.js
@@ -0,0 +1,9 @@
+let array = [5, 10];
+
+let resultadoPositivo = array.reduce((valorPrevio, valorAtual) => valorPrevio + valorAtual);
+
+let resultadoNegativo = array.reduce((valorPrevio, valorAtual) => valorPrevio - valorAtual);
+
+let resultadoMultiplicado = array.reduce((valorPrevio, valorAtual) => valorPrevio * valorAtual);
+
+let resultadoDividido = array.reduce((valorPrevio, valorAtual) => valorPrevio / valorAtual);
