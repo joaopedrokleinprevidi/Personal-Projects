ffcc632517838a24b4d23540d124a4bbda5ba858
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-12-07 08:51:45 -0300

    Update scripts.js

--- Blog API Project/js/scripts.js
+++ Blog API Project/js/scripts.js
@@ -1,70 +1,121 @@
-const url = "https://jsonplaceholder.typicode.com/posts"
+const url = "https://jsonplaceholder.typicode.com/posts";
 
-const loadingElement = document.querySelector("#loading")
-const postsContainer = document.querySelector("#posts-container")
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
 
-const postPage = document.querySelector("#post")
-const postContainer = document.querySelector("#post-container")
-const commentsContainer = document.querySelector("#comments-container")
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
 
-// Get id from URL
-const urlSearchParams = new URLSearchParams(window.location.search)
-//método que permite extrair dados da URL, no caso pegaremos o ID.
-const postID = urlSearchParams.get("id")
+const commentForm = document.querySelector("#comment-form");
+const emailInput = document.querySelector("#email");
+const bodyInput = document.querySelector("#body");
+
+// Load post
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
 
 // Get all posts
 async function getAllPosts() {
-    
-    const response = await fetch(url)
-    //aguardando chegar a requisição para a url ( Link da API )
-    
-    const data = await response.json()
-    //transformando os dados da API 
+  const response = await fetch(url);
 
-    loadingElement.classList.add("hide")
-    //escondendo "loading"
+  console.log(response);
 
-    data.map(post => {
-        const div = document.createElement("div")
-        const title = document.createElement("h2")
-        const body = document.createElement("p")
-        const link = document.createElement("a")
+  const data = await response.json();
 
-        title.innerText = post.title
-        body.innerText = post.body
-        link.innerText = "Ler"
-        link.setAttribute("href", `/post.html?id=${post.id}`)
+  console.log(data);
 
-        div.append(title, body, link)
-        postsContainer.appendChild(div)
-    })
-    //fazendo um map, para ele percorrer cada dado da API
-    //assim utilizando o DOM para colocar os dados na tela do usuário
-    //essa api está retornando pra nós os dados "title" e "body", que no caso são os dados que iremos usar
+  loadingElement.classList.add("hide");
+
+  data.map((post) => {
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const link = document.createElement("a");
+
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = "Ler";
+    link.setAttribute("href", `/post.html?id=${post.id}`);
+
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+    postsContainer.appendChild(div);
+  });
 }
 
 // Get individual post
-
 async function getPost(id) {
-    
-    //aqui estamos fazendo uma desestruturacao em um array, que contem a resposta de duas requisiçoes
-    //estamos fazendo as requisiçoes junto para economizar memoria do servidor e trafego de rede
-    //de maneira paralela acabamos economizando tempo também
-    //por isso utilizamos o await promise.all , que nos permite aguardar mais de uma requisiçao ( que sao feitas juntas , economizando tempo, memoria, e trafego de rede)
-    const [responsePost, responseComments] = await Promise.all([
-        fetch(`${url}/${id}`), fetch(`${url}/${id}/comments`)
-    ]) 
+  const [responsePost, responseComments] = await Promise.all([
+    fetch(`${url}/${id}`),
+    fetch(`${url}/${id}/comments`),
+  ]);
 
-    const dataPost = await responsePost.json()
+  const dataPost = await responsePost.json();
 
-    const dataComments = await responseComments.json()
+  const dataComments = await responseComments.json();
 
-    loadingElement.classList.add("hide")
-    postPage.classList.remove("hide")
+  loadingElement.classList.add("hide");
+  postPage.classList.remove("hide");
+
+  const title = document.createElement("h1");
+  const body = document.createElement("p");
+
+  title.innerText = dataPost.title;
+  body.innerText = dataPost.body;
+
+  postContainer.appendChild(title);
+  postContainer.appendChild(body);
+
+  dataComments.map((comment) => {
+    createComment(comment);
+  });
 }
 
-if (!postID) {
-    getAllPosts();
-}else {
-    getPost(postID)
+function createComment(comment) {
+  const div = document.createElement("div");
+  const email = document.createElement("h3");
+  const commentBody = document.createElement("p");
+
+  email.innerText = comment.email;
+  commentBody.innerText = comment.body;
+
+  div.appendChild(email);
+  div.appendChild(commentBody);
+  commentsContainer.appendChild(div);
+}
+
+// Insert a comment
+async function postComment(comment) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: comment,
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  createComment(data);
+}
+
+if (!postId) {
+  getAllPosts();
+} else {
+  getPost(postId);
+
+  commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let comment = {
+      email: emailInput.value,
+      body: bodyInput.value,
+    };
+
+    comment = JSON.stringify(comment);
+
+    postComment(comment);
+  });
 }
