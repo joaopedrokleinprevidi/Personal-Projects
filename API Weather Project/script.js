e9603c1f32ed3925106cfb306a454f08dbaf11db
João Pedro Klein Previdi <kleincoder@gmail.com>
2023-12-17 09:39:24 -0300

    Finish Open Weather API Project

--- API Weather Project/script.js
+++ API Weather Project/script.js
@@ -1,24 +1,34 @@
+const containerPrincipal = document.querySelector('#principalContainer')
 const inputCidade = document.querySelector('#nomeDaCidade');
 const buttonProcurar = document.querySelector('#iconSearch')
+const cidadeEscolhida = document.querySelector('#cidadeEscolhida')
+const temperaturaDaCidade = document.querySelector('#temperaturaDaCidade')
+const velocidadeDoVento = document.querySelector('#velocidadeDoVento')
+const iconeTemperaturaDaCidade = document.querySelector('#iconeTemperatura')
 let nomeDaCidade;
 
 buttonProcurar.addEventListener('click', () =>{
     console.log('clicou para buscar')
     nomeDaCidade = inputCidade.value;
     weather(nomeDaCidade)
-    inputCidade.value = '';
+    if (!nomeDaCidade == ''){
+    containerPrincipal.classList.toggle('principal-content-container')
+    containerPrincipal.classList.toggle('display-none')
+    }
 
+    inputCidade.value = '';
 })
 
 
 async function weather(nomeDaCidade) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${nomeDaCidade}&appid=4655fdf4d7fc72d811d4e2e4a143c991`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${nomeDaCidade}&units=metric&appid=4655fdf4d7fc72d811d4e2e4a143c991`);
         const data = await response.json();
 
         if (!response.ok) {
             throw new Error(response.status);
         }else {
+            carregarDadosProUsuario(data)
             console.log(data)
         }
 
@@ -27,4 +37,11 @@ async function weather(nomeDaCidade) {
     }
 }
 
-function carregarDadosProUsuario
+function carregarDadosProUsuario(data) {
+    cidadeEscolhida.innerText = data.name + ", " + data.sys.country
+    console.log(cidadeEscolhida.value)
+    temperaturaDaCidade.innerText = `Temperatura: ${Math.floor(data.main.temp)}° C` 
+    console.log(temperaturaDaCidade.innerText)
+    velocidadeDoVento.innerText =  `Vento: ${data.wind.speed} km/h`
+    iconeTemperaturaDaCidade.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+}
